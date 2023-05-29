80daa5f6f049fd789a3814642fa4293041eb263b
ss14740 <98390888+ss14740@users.noreply.github.com>
2023-05-29 23:48:27 +0400

    Add files via upload

---
+++ Assignment1/script.js
@@ -0,0 +1,49 @@
+//gets each element and applies mouse hover and exit
+var vid1 = document.getElementById("vid1");
+
+// Add event listener for mouseenter event
+vid1.addEventListener("mouseenter", function() {
+    vid1.play();
+});
+
+// Add event listener for mouseleave event
+vid1.addEventListener("mouseleave", function() {
+    vid1.pause();
+});
+
+var vid2 = document.getElementById("vid2");
+
+// Add event listener for mouseenter event
+vid2.addEventListener("mouseenter", function() {
+    vid2.play();
+});
+
+// Add event listener for mouseleave event
+vid2.addEventListener("mouseleave", function() {
+    vid2.pause();
+});
+
+var vid3 = document.getElementById("vid3");
+
+// Add event listener for mouseenter event
+vid3.addEventListener("mouseenter", function() {
+    vid3.play();
+});
+
+// Add event listener for mouseleave event
+vid3.addEventListener("mouseleave", function() {
+    vid3.pause();
+});
+
+var vid4 = document.getElementById("vid4");
+
+// Add event listener for mouseenter event
+vid4.addEventListener("mouseenter", function() {
+    vid4.play();
+});
+
+// Add event listener for mouseleave event
+vid4.addEventListener("mouseleave", function() {
+    vid4.pause();
+});
+
